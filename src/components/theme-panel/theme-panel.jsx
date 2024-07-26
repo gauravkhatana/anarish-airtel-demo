70f62d3730014ad455a7bd07a2e4ebdcfd42ecf8
Kumar Gaurav <gaurav.khatana@anarish.com>
2024-07-26 15:18:41 +0530

    updated vercel-config

--- src/components/theme-panel/theme-panel.jsx
+++ src/components/theme-panel/theme-panel.jsx
@@ -172,19 +172,19 @@ function ThemePanel() {
                                   </button>
                                   <ul className="dropdown-menu w-100">
                                     <li>
-                                      <a className="dropdown-item" href="/">
+                                      
                                         Action
-                                      </a>
+                                      
                                     </li>
                                     <li>
-                                      <a className="dropdown-item" href="/">
+                                      
                                         Another action
-                                      </a>
+                                      
                                     </li>
                                     <li>
-                                      <a className="dropdown-item" href="/">
+                                      
                                         Something else here
-                                      </a>
+                                      
                                     </li>
                                   </ul>
                                 </div>
