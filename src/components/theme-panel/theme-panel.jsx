ecf1243ebfd750dbbc687c7e2d8c767382e8daa6
Kumar Gaurav <gaurav.khatana@anarish.com>
2024-07-26 14:03:27 +0530

    updated href

--- src/components/theme-panel/theme-panel.jsx
+++ src/components/theme-panel/theme-panel.jsx
@@ -172,17 +172,17 @@ function ThemePanel() {
                                   </button>
                                   <ul className="dropdown-menu w-100">
                                     <li>
-                                      <a className="dropdown-item" href="#">
+                                      <a className="dropdown-item" href="#!">
                                         Action
                                       </a>
                                     </li>
                                     <li>
-                                      <a className="dropdown-item" href="#">
+                                      <a className="dropdown-item" href="#!">
                                         Another action
                                       </a>
                                     </li>
                                     <li>
-                                      <a className="dropdown-item" href="#">
+                                      <a className="dropdown-item" href="#!">
                                         Something else here
                                       </a>
                                     </li>
